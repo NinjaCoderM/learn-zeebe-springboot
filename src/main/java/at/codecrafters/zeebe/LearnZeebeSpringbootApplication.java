29b955f5f234401197eaac01181ec81b9fab862f
wienm <martin.scheibelreiter@chello.at>
2025-05-01 22:27:08 +0200

    init, Springboot, spring-zeebe-starter

---
+++ src/main/java/at/codecrafters/zeebe/LearnZeebeSpringbootApplication.java
@@ -0,0 +1,13 @@
+package at.codecrafters.zeebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnZeebeSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnZeebeSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/codecrafters/zeebe/LearnZeebeSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package at.codecrafters.zeebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnZeebeSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
